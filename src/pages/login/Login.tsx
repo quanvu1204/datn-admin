ef267bf6cdc20885f4b5927eb07d349c79393d28
quan.vutien <quan.vutien@powergatesoftware.net>
2021-05-04 14:56:44 +0700

    update code

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,74 @@
 import React from 'react';
+import { Form, Input, Button, Row, Col, notification } from 'antd';
+
+import { storage } from '../../common/utils/storage';
+import services from '../../common/services/services';
+
+import './LoginStyles.scss';
 
 const Login: React.FunctionComponent = () => {
-    return <div>Login</div>;
+    const tailLayout = {
+        wrapperCol: { offset: 8, span: 16 },
+    };
+
+    const onFinish = async (values: any) => {
+        try {
+            const response = await services.login(values);
+            if (response.code === 200 && response.data?.token) {
+                storage.setToken(response.data?.token, 'token');
+                return window.location.reload();
+            }
+            return notification.error({
+                message: 'Lỗi',
+                description: 'Vui lòng thử lại!',
+                duration: 2,
+            });
+        } catch (error) {
+            notification.error({
+                message: 'Lỗi',
+                description: 'Vui lòng thử lại!',
+                duration: 2,
+            });
+        }
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return (
+        <Row>
+            <Col span={12} offset={6}>
+                <Form
+                    name="basic"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    className="form-login"
+                >
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[{ required: true, message: 'Please input your email!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+                    <Form.Item {...tailLayout}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Col>
+        </Row>
+    );
 };
 
 export default Login;
