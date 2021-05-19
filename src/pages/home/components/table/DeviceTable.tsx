c278c4b1b8af75b0d5bf6c40972fedc783245c9c
quan.vutien <quan.vutien@powergatesoftware.net>
2021-05-19 23:13:04 +0700

    update code

--- src/pages/home/components/table/DeviceTable.tsx
+++ src/pages/home/components/table/DeviceTable.tsx
@@ -1,8 +1,20 @@
 /* eslint-disable react/display-name */
 import React, { useEffect, useState } from 'react';
 import { Table } from 'antd';
+import firebase from 'firebase';
 
 import services from '../../../../common/services/services';
+
+export const firebaseConfig = {
+    apiKey: 'AIzaSyDSn-cNFh08Y9a9Dyh2Gaw4aYhDtQtPU6U',
+    authDomain: 'beem-assistant.firebaseapp.com',
+    databaseURL: 'https://beem-assistant-default-rtdb.firebaseio.com',
+    projectId: 'beem-assistant',
+    storageBucket: 'beem-assistant.appspot.com',
+    messagingSenderId: '461158918323',
+    appId: '1:461158918323:web:360aac9f14e9bc23c7e8c6',
+    measurementId: 'G-KPG3X9JM89',
+};
 
 interface DeviceTable {
     key: string;
@@ -13,7 +25,15 @@ interface DeviceTable {
 const DeviceTable: React.FunctionComponent = () => {
     const [data, setData] = useState<DeviceTable[]>([]);
 
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
+
     const handleDelete = async (item: DeviceTable) => {
+        await firebase
+            .database()
+            .ref(`/devices/${item.ip.split('.').join('-')}`)
+            .remove();
         const response = await services.delDevice(item.key);
         if (response.code === 200) {
             getListDevice();
